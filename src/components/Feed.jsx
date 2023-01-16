13ab988961e3d68c4585d5f1d9e753eeb45150ff
shivam <jhodshivam5122@gmail.com>
2023-01-17 02:03:34 +0530

    videocard

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,26 @@
 import { useEffect,useState } from "react"
 import { Box, Stack, Typography } from "@mui/material"
 import {Videos,Sidebar} from './'
+import { fetchFromAPI } from './../utils/fetchFromAPI';
+
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then(( data ) => setVideos(data.items))
+  },[selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column",md: "row"} }}>
       <Box sx={{ height: {sx: 'auto',md:'92vh'},borderRight:'1px solid #3d3d3d',px:{sx: 0, md:2} }}>
-        <Sidebar/>
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
 
         <Typography className="copyright" variant="body2" sx={{ mt:1.5,color:'#fff' }}>
           copyright 2022 shivamjhod
@@ -15,10 +29,11 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY:'auto' ,height:'90vh' ,flex:2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{color:'white'}}>
-          New <span style={{ color:'#F31503' }} >video</span>
+          {selectedCategory} <span style={{ color:'#F31503' }} >videos</span>
         </Typography>
+      
+      <Videos videos={videos}/>
       </Box>
-      <Videos/>
     </Stack>
   )
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,9 +1,9 @@
 import { Stack } from "@mui/material"
 import { categories } from "../utils/constants"
 
-const selectCategory = 'New';
 
-const Sidebar = () => (
+
+const Sidebar = ({ selectedCategory,setSelectedCategory}) => (
  <Stack
         direction="row"
         sx={{
@@ -16,14 +16,15 @@ const Sidebar = () => (
         {categories.map((category) => (
             <button 
                 className="category-btn"
+                onClick={() => setSelectedCategory(category.name)}
                 style={{
-                    background: category.name === selectCategory && '#FC1503',
+                    background: category.name === selectedCategory && '#FC1503',
                     color:'white'
                 }}
                 key={categories.name}
             >
-                <span style={{ color:category.name === selectCategory ? 'white' : "red" ,marginRight:'15px' }} >{category.icon}</span>
-                <span style={{ opacity: category.name === selectCategory ? '1':'0.8' }}>{category.name}</span>
+                <span style={{ color:category.name === selectedCategory ? 'white' : "red" ,marginRight:'15px' }} >{category.icon}</span>
+                <span style={{ opacity: category.name === selectedCategory ? '1':'0.8' }}>{category.name}</span>
             </button>
         ))}
     </Stack>
